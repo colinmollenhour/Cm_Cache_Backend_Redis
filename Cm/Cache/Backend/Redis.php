bef5fb6a29f05cdcb4d05a5d490ee8f59b5634a8
Constantin Bejenaru <constantin.bejenaru@evozon.com>
2014-05-30 18:14:10 +0300

    Added previous commit's value also to the lua script in _removeByMatchingAnyTags

--- Cm/Cache/Backend/Redis.php
+++ Cm/Cache/Backend/Redis.php
@@ -436,7 +436,7 @@ class Cm_Cache_Backend_Redis extends Zend_Cache_Backend implements Zend_Cache_Ba
             $sArgs = array(self::PREFIX_KEY, self::SET_TAGS, self::SET_IDS, ($this->_notMatchingTags ? 1 : 0));
             if ( ! $this->_redis->evalSha(self::LUA_CLEAN_SH1, $pTags, $sArgs)) {
                 $script =
-                    "local steps = " . $this->_luaKeySteps . " ".
+                    "local steps = " . $this->_luaMaxCStack . " ".
                     "while #KEYS > 0 do ".
                         "local i = 0 ".
                         "local keysToProcess = {} ".
