b7bdf33d11646256b88bedb8cf221eed8ec17f07
Constantin Bejenaru <constantin.bejenaru@evozon.com>
2014-06-02 13:06:42 +0300

    Updated loop for KEYS processing and use unpack() args instead of creating additional table

--- Cm/Cache/Backend/Redis.php
+++ Cm/Cache/Backend/Redis.php
@@ -436,23 +436,17 @@ class Cm_Cache_Backend_Redis extends Zend_Cache_Backend implements Zend_Cache_Ba
             $sArgs = array(self::PREFIX_KEY, self::SET_TAGS, self::SET_IDS, ($this->_notMatchingTags ? 1 : 0));
             if ( ! $this->_redis->evalSha(self::LUA_CLEAN_SH1, $pTags, $sArgs)) {
                 $script =
-                    "local steps = " . $this->_luaMaxCStack . " ".
-                    "while #KEYS > 0 do ".
-                        "local i = 0 ".
-                        "local keysToProcess = {} ".
-                        "while i < steps and #KEYS > 0 do ".
-                            "table.insert(keysToProcess, table.remove(KEYS)) ".
-                            "i = i + 1 ".
-                        "end ".
-                        "local keysToDel = redis.call('SUNION', unpack(keysToProcess)) ".
+                    "local steps = " . (int) $this->_luaMaxCStack . " ".
+                    "for i = 1, #KEYS, steps do ".
+                        "local keysToDel = redis.call('SUNION', unpack(KEYS, i, math.min(#KEYS, i + steps - 1))) ".
                         "for _, keyname in ipairs(keysToDel) do ".
                             "redis.call('DEL', ARGV[1]..keyname) ".
                             "if (ARGV[4] == '1') then ".
                                 "redis.call('SREM', ARGV[3], keyname) ".
                             "end ".
                         "end ".
-                        "redis.call('DEL', unpack(keysToProcess)) ".
-                        "redis.call('SREM', ARGV[2], unpack(keysToProcess)) ".
+                        "redis.call('DEL', unpack(KEYS, i, math.min(#KEYS, i + steps - 1))) ".
+                        "redis.call('SREM', ARGV[2], unpack(KEYS, i, math.min(#KEYS, i + steps - 1))) ".
                     "end ".
                     "return true";
                 $this->_redis->eval($script, $pTags, $sArgs);
